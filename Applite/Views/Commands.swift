6025f185af65156e3d1db0022a12237f9996798c
Tom Theret <tomnils.theret@gmail.com>
2023-08-14 14:23:37 +0200

    Upgrade command bar
    - Changing the about window
    - MacOS wiki button removal
    - Added discord link
    - Added link to github repo

--- Applite/Views/Commands.swift
+++ Applite/Views/Commands.swift
@@ -16,6 +16,25 @@ struct CommandsMenu: Commands {
     var body: some Commands {
         SidebarCommands()
         
+        CommandGroup(replacing: .appInfo) {
+                        Button("About \(Bundle.main.appName)") {
+                            NSApplication.shared.orderFrontStandardAboutPanel(
+                                options: [
+                                    NSApplication.AboutPanelOptionKey.credits: NSAttributedString(
+                                        string: "MIT Licence",
+                                        attributes: [
+                                            NSAttributedString.Key.font: NSFont.systemFont(
+                                                ofSize: NSFont.smallSystemFontSize)
+                                        ]
+                                    ),
+                                    NSApplication.AboutPanelOptionKey(
+                                        rawValue: "Copyright"
+                                    ): "© 2023 Milán Várady"
+                                ]
+                            )
+                        }
+                    }
+        
         CommandGroup(before: .systemServices) {
             Button("Uninstall...") {
                 openWindow(id: "uninstall-self")
@@ -27,10 +46,16 @@ struct CommandsMenu: Commands {
             
             Divider()
         }
+
         
-        CommandGroup(before: .help) {
+        CommandGroup (replacing: .newItem) {
+        }
+        
+        CommandGroup(replacing: .help) {
             Link("Website", destination: URL(string: "https://aerolite.dev/applite")!)
             Link("Troubleshooting", destination: URL(string: "https://aerolite.dev/applite/troubleshooting.html")!)
+            Link("Github", destination: URL(string: "https://github.com/milanvarady/Applite")!)
+            Link("Discord", destination: URL(string: "https://discord.gg/ZgB6pRE8Qx")!)
         }
     }
 }
