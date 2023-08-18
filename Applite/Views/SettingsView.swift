a337ac45b7cfbd00623d2f725ad3377c7dcfead0
Várady Milán <61704770+MilanVarady@users.noreply.github.com>
2023-08-18 14:20:03 +0200

    Fix color scheme not changing
    These changes are made because changing the .preferredColorScheme view modifier is bugged. When it's set back to nil, parts of the UI don't default back to the system color scheme.
    
    Small fixes for the Hungarian translation.

--- Applite/Views/SettingsView.swift
+++ Applite/Views/SettingsView.swift
@@ -62,15 +62,17 @@ fileprivate struct GeneralSettingsView: View {
     @AppStorage("notificationSuccess") var notificationOnSuccess: Bool = false
     @AppStorage("notificationFailure") var notificationOnFailure: Bool = true
 
+    /// Needed for a workaround for changing the color scheme
+    @State var fixingColor = false
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text("Appearance")
                 .bold()
 
             Picker("Color Scheme:", selection: $colorSchemePreference) {
-
                 ForEach(ColorSchemePreference.allCases) { color in
-                    Text(NSLocalizedString(color.rawValue.capitalized, comment: "Colors schemes names"))
+                    Text(NSLocalizedString(color.rawValue.capitalized, comment: "Color scheme names"))
                 }
             }
             .pickerStyle(.segmented)
@@ -85,6 +87,31 @@ fileprivate struct GeneralSettingsView: View {
             Toggle("Task errors", isOn: $notificationOnFailure)
         }
         .padding()
+        .onChange(of: colorSchemePreference) {
+            // Don't remove this!
+            // This is here because changing the .preferredColorScheme view modifier is bugged
+            // When it's set back to nil, parts of the UI don't default back to the system color scheme
+            if $0 == .system && !fixingColor {
+                // Set fixingColor to true, so we don't recursively call this function
+                self.fixingColor = true
+                
+                // Get system color scheme
+                let darkMode = UserDefaults.standard.string(forKey: "AppleInterfaceStyle") == "Dark"
+                
+                Task {
+                    // Set color scheme to system
+                    colorSchemePreference = darkMode ? .dark : .light
+                    // Wait
+                    try? await Task.sleep(for: .seconds(0.001))
+                    // Set it back to nil (.system)
+                    colorSchemePreference = .system
+                    // Wait
+                    try? await Task.sleep(for: .seconds(0.001))
+                    // Set fixing color back to false
+                    await MainActor.run { self.fixingColor = false }
+                }
+            }
+        }
     }
 }
 
