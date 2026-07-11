8a47fdc485ababc3f4581ca463d9dbc34ff0c529
Milán Várady <varady.misi@gmail.com>
2026-07-11 14:15:27 +0200

    Fix sidebar width

--- Applite/Navigation/ContentView.swift
+++ Applite/Navigation/ContentView.swift
@@ -62,6 +62,7 @@ struct ContentView: View {
         return NavigationSplitView {
             SidebarView(selection: $selection)
                 .disabled(modifyingBrew)
+                .navigationSplitViewColumnWidth(216)
         } detail: {
             if caskManager.hasBrokenInstall && !showSetup {
                 BrokenInstallView()
